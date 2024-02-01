aa5b70da80c7df8f7d3d597ead582ceb5b326603
matteomedori <matteo.medori.97@gmail.com>
2024-02-01 15:22:31 +0100

    feat: aggiunta migration

---
+++ database/migrations/2024_02_01_140732_create_comics_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    
+    public function up(): void
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title',70);
+            $table->text('description');
+            $table->string('thumb',400);
+            $table->decimal('price', 6, 2);
+            $table->string('series',50);
+            $table->date('sale_date');
+            $table->string('type',50);
+            $table->json('artists');
+            $table->json('writers');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comics');
+    }
+};
